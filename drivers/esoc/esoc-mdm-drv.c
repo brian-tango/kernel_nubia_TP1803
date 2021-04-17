d009404bbe0b234a41fa77d8a680ab02b469bf79
LibXZR <xzr467706992@163.com>
2021-04-17 13:04:45 +0800

    [PATCH] [HACK] esoc & mhi & subsystem_restart: Don't crash when modem  restart failed
    * Todo: Fix this completely
    
    Signed-off-by: zclkkk <2484392714@qq.com>

--- drivers/esoc/esoc-mdm-drv.c
+++ drivers/esoc/esoc-mdm-drv.c
@@ -411,14 +411,11 @@ static int mdm_handle_boot_fail(struct esoc_clink *esoc_clink, u8 *pon_trial)
 		mdm_power_down(mdm);
 		msleep(S3_RESET_DELAY_MS);
 		break;
-	case BOOT_FAIL_ACTION_PANIC:
-		esoc_mdm_log("Calling panic!!\n");
-		panic("Panic requested on external modem boot failure\n");
-		break;
 	case BOOT_FAIL_ACTION_NOP:
 		esoc_mdm_log("Leaving the modem in its curent state\n");
 		return -EIO;
 	case BOOT_FAIL_ACTION_SHUTDOWN:
+	case BOOT_FAIL_ACTION_PANIC:
 	default:
 		mdm_subsys_retry_powerup_cleanup(esoc_clink,
 							ESOC_HOOK_MDM_DOWN);
